a84d4aa3e9606092ae89e4e4a02e030eb38246cc
haozijie07 <haozijie07@gmail.com>
2025-05-26 23:08:14 +0800

    feat(table): 为表格搜索组件添加更多检索功能
    - 在搜索表单中添加"更多检索"按钮，点击后显示更多搜索条件
    - 新增抽屉组件用于展示更多搜索条件，包括各种类型的输入框和选择框
    - 优化搜索表单布局，使用 v-show 替代 v-if 提高渲染效率
    - 添加重置和搜索按钮到抽屉底部
    - 调整搜索表单样式，使界面更加美观

--- src/components/advancedComponents/table/SearchForm/index.vue
+++ src/components/advancedComponents/table/SearchForm/index.vue
@@ -3,7 +3,7 @@
     <el-form ref="formRef" :model="searchFormData" label-width="auto">
       <el-row :gutter="20">
         <template v-for="(searchItem, searchIndex) in tableSearchComputed" :key="searchItem.field">
-          <el-col v-if="searchIndex < 6" :xs="24" :sm="12" :md="12" :lg="8" :xl="8">
+          <el-col v-show="searchIndex < 6" :xs="24" :sm="12" :md="12" :lg="8" :xl="8">
             <el-form-item
               :label="searchItem.title"
               :prop="`${searchItem.field}_value`"
@@ -143,9 +143,162 @@
       </el-row>
       <el-form-item>
         <el-button type="primary" @click="handleSearch">搜索</el-button>
+        <el-button type="primary" plain @click="handleShowMorCondition">更多检索</el-button>
         <el-button type="info" @click="handleReset">重置</el-button>
       </el-form-item>
     </el-form>
+    <el-drawer header-class="el-drawer-header" v-model="moreConditionVisible" :size="'50%'">
+      <template #header>
+        <div class="el-drawer-title">更多检索</div>
+      </template>
+      <el-form ref="moreConditionFormRef" :model="searchFormData" label-width="auto">
+        <el-row :gutter="20">
+          <template v-for="searchItem in tableSearchComputed" :key="searchItem.field">
+            <el-col>
+              <el-form-item
+                :label="searchItem.title"
+                :prop="`${searchItem.field}_value`"
+                :rules="searchItem.rules"
+              >
+                <div style="display: flex; width: 100%">
+                  <el-select
+                    style="width: 120px; margin: 0 10px; flex-shrink: 0"
+                    v-model="searchFormData[`${searchItem.field}_operator`]"
+                    :placeholder="searchItem.placeholder"
+                    v-bind="searchItem.itemProps"
+                  >
+                    <el-option
+                      v-for="item in operatorMap.get(searchItem.type)"
+                      :label="item.label"
+                      :value="item.value"
+                      :key="item.value"
+                    >
+                    </el-option>
+                  </el-select>
+                  <template v-if="searchItem.type === 'text'">
+                    <el-input
+                      style="flex-grow: 1"
+                      v-model="searchFormData[`${searchItem.field}_value`]"
+                      :placeholder="searchItem.placeholder"
+                      v-bind="searchItem.itemProps"
+                    />
+                  </template>
+                  <template v-else-if="searchItem.type === 'number'">
+                    <el-input-number
+                      style="flex-grow: 1"
+                      v-model="searchFormData[`${searchItem.field}_value`]"
+                      :min="-99999"
+                      :max="99999"
+                      :placeholder="searchItem.placeholder"
+                      v-bind="searchItem.itemProps"
+                    />
+                  </template>
+                  <template v-else-if="searchItem.type === 'select'">
+                    <el-select
+                      v-model="searchFormData[`${searchItem.field}_value`]"
+                      :placeholder="searchItem.placeholder"
+                      v-bind="searchItem.itemProps"
+                    >
+                      <el-option
+                        v-for="item in searchItem.options"
+                        :label="item.label"
+                        :value="item.value"
+                        :key="item.value"
+                      />
+                    </el-select>
+                  </template>
+                  <template v-else-if="searchItem.type === 'multiselect'">
+                    <el-select
+                      v-model="searchFormData[`${searchItem.field}_value`]"
+                      :placeholder="searchItem.placeholder"
+                      :multiple="true"
+                      v-bind="searchItem.itemProps"
+                    >
+                      <el-option
+                        v-for="item in searchItem.options"
+                        :label="item.label"
+                        :value="item.value"
+                        :key="item.value"
+                      />
+                    </el-select>
+                  </template>
+                  <template v-else-if="searchItem.type === 'radio'">
+                    <el-radio-group
+                      v-model="searchFormData[`${searchItem.field}_value`]"
+                      v-bind="searchItem.itemProps"
+                    >
+                      <el-radio
+                        v-for="item in searchItem.options"
+                        :value="item.value"
+                        :key="item.value"
+                      >
+                        {{ item.label }}
+                      </el-radio>
+                    </el-radio-group>
+                  </template>
+                  <template v-else-if="searchItem.type === 'checkbox'">
+                    <el-checkbox-group
+                      v-model="searchFormData[`${searchItem.field}_value`]"
+                      v-bind="searchItem.itemProps"
+                    >
+                      <el-checkbox-button
+                        v-for="item in searchItem.options"
+                        :value="item.value"
+                        :key="item.value"
+                        :label="item.label"
+                      />
+                    </el-checkbox-group>
+                  </template>
+                  <template v-else-if="searchItem.type === 'date'">
+                    <el-date-picker
+                      v-model="searchFormData[`${searchItem.field}_value`]"
+                      type="date"
+                      :placeholder="searchItem.placeholder"
+                      style="flex-grow: 1"
+                      v-bind="searchItem.itemProps"
+                    />
+                  </template>
+                  <template v-else-if="searchItem.type === 'datetime'">
+                    <el-date-picker
+                      v-model="searchFormData[`${searchItem.field}_value`]"
+                      type="datetime"
+                      :placeholder="searchItem.placeholder"
+                      style="flex-grow: 1"
+                      v-bind="searchItem.itemProps"
+                    />
+                  </template>
+                  <template v-else-if="searchItem.type === 'daterange'">
+                    <el-date-picker
+                      v-model="searchFormData[`${searchItem.field}_value`]"
+                      type="daterange"
+                      :start-placeholder="searchItem.startPlaceholder"
+                      :end-placeholder="searchItem.endPlaceholder"
+                      style="flex-grow: 1"
+                      v-bind="searchItem.itemProps"
+                    />
+                  </template>
+                  <template v-else-if="searchItem.type === 'datetimerange'">
+                    <el-date-picker
+                      v-model="searchFormData[`${searchItem.field}_value`]"
+                      type="datetimerange"
+                      :start-placeholder="searchItem.startPlaceholder"
+                      :end-placeholder="searchItem.endPlaceholder"
+                      style="flex-grow: 1"
+                      v-bind="searchItem.itemProps"
+                    />
+                  </template>
+                </div>
+              </el-form-item>
+            </el-col>
+          </template>
+        </el-row>
+        <el-form-item> </el-form-item>
+      </el-form>
+      <template #footer>
+        <el-button type="info" @click="handleReset">重置</el-button>
+        <el-button type="primary" @click="handleSearch">搜索</el-button>
+      </template>
+    </el-drawer>
   </div>
 </template>
 
@@ -189,6 +342,7 @@ const emits = defineEmits<{
 }>()
 
 const formRef = ref<InstanceType<typeof ElForm>>()
+const moreConditionFormRef = ref<InstanceType<typeof ElForm>>()
 
 const searchFormData = defineModel<any>('searchFormData', { default: {} })
 
@@ -252,6 +406,12 @@ function handleReset() {
   emits('onReset')
 }
 
+/* ---------------------------------- 更多检索 ---------------------------------- */
+const moreConditionVisible = ref(false)
+function handleShowMorCondition() {
+  moreConditionVisible.value = true
+}
+
 onMounted(() => {
   initSearchFormData()
   handleSearch()
@@ -268,5 +428,15 @@ defineExpose({
 .haozi-table-search {
   flex-shrink: 0;
   overflow: hidden;
+}
+</style>
+
+<style lang="scss">
+.el-drawer-header {
+  margin-bottom: 0;
+  .el-drawer-title {
+    color: rgb(17, 17, 17);
+    font-weight: 600;
+  }
 }
 </style>
