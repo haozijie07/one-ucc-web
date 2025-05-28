dfdf35f6394b3e0d295c55c929ac20005f92b1bb
haozijie07 <haozijie07@gmail.com>
2025-05-28 22:40:35 +0800

    feat: 添加深色主题样式
    - 在 App.vue 中引入 getDarkColor 函数
    - 为 body 元素添加深色主题的 CSS 变量

--- src/App.vue
+++ src/App.vue
@@ -8,9 +8,10 @@
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
 import { useChangeColor } from '@/utils/calc-color'
 
-const { getLightColor } = useChangeColor()
+const { getLightColor, getDarkColor } = useChangeColor()
 for (let i = 1; i <= 9; i++) {
   document.body.style.setProperty(`--el-color-primary-light-${i}`, getLightColor('#4caf50', i / 10))
+  document.body.style.setProperty(`--el-color-primary-dark-${i}`, getDarkColor('#4caf50', i / 10))
 }
 </script>
 
