c553d8dec94297b0a8b7052f7c660015d6989680
haozijie07 <haozijie07@gmail.com>
2025-06-06 23:41:22 +0800

    refactor(request): 更新 API 基础 URL
    - 在 request/index.ts 文件中修改了 uccAxios 实例的 baseURL 配置
    - 将 baseURL 从 'http://localhost:3000' 修改为 'http://localhost:3000/api'
    - 这个改动使得所有 API 请求都会自动添加 '/api' 前缀，简化了请求路径的管理

--- src/request/index.ts
+++ src/request/index.ts
@@ -1,6 +1,6 @@
 import { createAxiosInstance } from './baseRequest'
 
 export const uccAxios = createAxiosInstance({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'http://localhost:3000/api',
   timeout: 10000,
 })
